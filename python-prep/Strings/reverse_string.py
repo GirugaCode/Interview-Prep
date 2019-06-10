ca3b40ebaffcaf48325bef75e93dfb19028e2471
Ryan Nguyen <ryan.nguyen@students.makeschool.com>
2019-06-10 10:05:12 -0700

    [add] Communication on reverse_strings

--- python-prep/Strings/reverse_string.py
+++ python-prep/Strings/reverse_string.py
@@ -5,42 +5,54 @@ Do not allocate extra space for another array, you must do this by modifying the
 
 You may assume all the characters consist of printable ascii characters.
 
- 
+Communication Steps:
+
+Rephrase Question:
+Okay, so I am given a list of strings and I have to reverse without modifying space complexity
+by moving it to another array for example. All characters in the list will be ascii characters.
+
+Questions:
+Would there be a possible for the array of strings to be empty?
+Does the output have to be case-sensitive to the input?
+Since I have to keep O(1) time in memory, will I have to account that rule for
+space complexity as well?
+
+Assumptions:
+I can safely assume that I can not use built-in functions.
+I should account for empty strings in the list.
+The output should clearly reflect the case-sensitivity.
+I need to keep O(1) in time in memory but I should not worry about
+the space complexity.
 
 Example 1:
-
 Input: ["h","e","l","l","o"]
 Output: ["o","l","l","e","h"]
 Example 2:
 
 Input: ["H","a","n","n","a","h"]
 Output: ["h","a","n","n","a","H"]
+
+
+
 """
 
-# def reverseString(string):
-#     index = 0
-#     string_size = len(string) -1
-
-#     while index < string_size:
-#         string[index], string[string] = string[string_size], string[index]
-#         index += 1
-#         string_size -= 1
-#     return string
 
 
 
 
+def reverseString(array):
 
-def reverseString(s):
     """
     :type s: List[str]
     :rtype: void Do not return anything, modify s in-place instead.
     """
-    index = 0
-    last_index = len(s) - 1
-    while index <= last_index:
-        s[index], s[last_index] = s[last_index], s[index]
-        index += 1
+    first_index = 0
+    last_index = len(array) - 1
+    while first_index <= last_index:
+        array[first_index], array[last_index] = array[last_index], array[first_index]
+        first_index += 1
         last_index -= 1
+
+    return array
 
 print(reverseString(["h","e","l","l","o"]))
